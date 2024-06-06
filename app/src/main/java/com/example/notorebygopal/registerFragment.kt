28c578b212032240d34bee02388f8f36e337da9b
Gopal Gupta <k7523337@gmail.com>
2024-06-06 09:34:19 +0530

    Notore

--- app/src/main/java/com/example/notorebygopal/registerFragment.kt
+++ app/src/main/java/com/example/notorebygopal/registerFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
